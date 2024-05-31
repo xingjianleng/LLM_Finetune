6a9cfd7836337c479966c911cda6a8ad53c270c1
Xingjian Leng <xingjianleng@gmail.com>
2024-06-01 00:42:39 +1000

    updatenew ultrachat200k script

--- scripts/llama3-8b_sft_ultrachat200k.sh
+++ scripts/llama3-8b_sft_ultrachat200k.sh
@@ -13,16 +13,16 @@ CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file configs/accelerate_conf
     --peft_lora_r 64 \
     --peft_lora_alpha 16 \
     --max_seq_length 2048 \
-    --peft_lora_dropout 0.0 \
-    --peft_use_rslora true \
+    --peft_lora_dropout 0.1 \
+    --peft_use_rslora false \
     --peft_lora_bias none \
-    --peft_lora_targets q_proj,v_proj \
+    --peft_lora_targets q_proj,k_proj,v_proj,o_proj \
     --train_split train_sft \
     --test_split test_sft \
     --per_device_train_batch_size 8 \
     --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps 8 \
-    --num_train_epochs 3 \
+    --num_train_epochs 1 \
     --output_dir output/llama3-8b_sft_ultrachat200k \
     --optim adamw_torch \
     --lr_scheduler_type cosine \
@@ -32,8 +32,6 @@ CUDA_VISIBLE_DEVICES=0,1 accelerate launch --config_file configs/accelerate_conf
     --save_steps 800 \
     --save_total_limit 2 \
     --logging_steps 1 \
-    --loraplus_lr_ratio 16.0 \
-    --loraplus_lr_embedding 1e-6 \
     --warmup_ratio 0.03 \
     --bf16 true \
     --packing true \
