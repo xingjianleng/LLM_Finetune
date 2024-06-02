96cfd7b300e007c6504a44472c35b7293f4bc1df
Xingjian Leng <xingjianleng@gmail.com>
2024-06-02 21:58:27 +1000

    Accidentally changed batch size

--- scripts/llama3-8b_sft_ultrachat200k.sh
+++ scripts/llama3-8b_sft_ultrachat200k.sh
@@ -29,8 +29,8 @@ deepspeed --master_port $MASTER_PORT src/sft_trainer.py \
     --peft_lora_targets q_proj,k_proj,v_proj,o_proj \
     --train_split train_sft \
     --test_split test_sft \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps 8 \
     --gradient_checkpointing true \
     --learning_rate 5e-5 \
